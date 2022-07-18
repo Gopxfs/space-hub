1b32c3f53860522511ae5663a96f1c2381b17349
Gabriel <gopxfs@hotmail.com>
2022-07-18 14:24:45 -0300

    Initial setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import { Routes, Route } from 'react-router-dom';
+
+class App extends React.PureComponent {
+  render() {
+    return (
+      <>
+      </>
+    );
+  }
+}
+
+export default App;
