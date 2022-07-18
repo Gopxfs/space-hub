bca9a200b9b855c4722cac48a016047fe3ccf7b1
Gabriel <gopxfs@hotmail.com>
2022-07-18 14:40:29 -0300

    Add routes to pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
-// import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import RocketsPage from './pages/Rockets';
+import MissionsPage from './pages/Missions';
+import ProfilePage from './pages/Profile';
 
 class App extends React.PureComponent {
   render() {
     return (
       <>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<RocketsPage />} />
+          <Route path='/rockets' element={<RocketsPage />} />
+          <Route path='/missions' element={<MissionsPage />} />
+          <Route path='/profile' element={<ProfilePage />} />
+        </Routes>
       </>
     );
   }
