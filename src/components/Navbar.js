74af7cf3a72396a1b573ef23ad41c0eb98db3cc0
worashf <uppert83@gmail.com>
2022-07-19 01:37:09 +0300

    style nav with react bootstrap

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,38 @@
 import { NavLink } from 'react-router-dom';
+import { Nav, Navbar, Container } from 'react-bootstrap';
 import logo from '../images/logo.png';
 
-const Navbar = () => (
-  <nav>
-    <div>
-      <img src={logo} height="50" alt="Logo" />
-      <p>Space Travelers&apos; Hub</p>
-    </div>
-    <ul>
-      <li>
-        <NavLink to="/rockets">Rockets</NavLink>
-      </li>
-      <li>
-        <NavLink to="/missions">Missions</NavLink>
-      </li>
-      <li>
-        <NavLink to="/profile">My Profile</NavLink>
-      </li>
-    </ul>
-  </nav>
+const brandStyle = {
+  display: 'flex',
+};
+
+const NavBar = () => (
+  <Navbar bg="light" variant="light" className="justify-content-between">
+    <Container fluid>
+      <Navbar.Brand style={brandStyle}>
+        <img src={logo} height="50" alt="Logo" className="me-3" />
+        <p>Space Travelers Hub</p>
+      </Navbar.Brand>
+      <Nav>
+        <Nav.Link>
+          <NavLink to="/rockets" style={{ textDecoration: 'none' }}>
+            Rockets
+          </NavLink>
+        </Nav.Link>
+
+        <Nav.Link>
+          <NavLink to="/missions" style={{ textDecoration: 'none' }}>
+            Missions
+          </NavLink>
+        </Nav.Link>
+        <Nav.Link>
+          <NavLink to="/profile" style={{ textDecoration: 'none' }}>
+            My Profile
+          </NavLink>
+        </Nav.Link>
+      </Nav>
+    </Container>
+  </Navbar>
 );
 
-export default Navbar;
+export default NavBar;
