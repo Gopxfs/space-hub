d1685915211ac8a2d1fba077300740f534b5dbc8
worashf <uppert83@gmail.com>
2022-07-19 22:01:49 +0300

    add  singlr rocket item componets

---
+++ src/components/RocketItem.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  Row, Col, Card, Button,
+} from 'react-bootstrap';
+import './Rocket.css';
+
+const RocketItem = (props) => {
+  const { name, thumb, description } = props;
+  return (
+    <>
+      <Card className="mb-2">
+        <Row>
+          <Col lg={4} md={4}>
+            <Card.Img src={thumb} className="thumbnail" />
+          </Col>
+
+          <Col lg={8} md={8}>
+            <Card.Body>
+              <Card.Title>
+                {' '}
+                {name}
+              </Card.Title>
+              <Card.Text>
+                {' '}
+                {description}
+                {' '}
+              </Card.Text>
+              <Button variant="primary">Goes some</Button>
+            </Card.Body>
+          </Col>
+        </Row>
+      </Card>
+    </>
+  );
+};
+
+RocketItem.propTypes = {
+  // id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  thumb: PropTypes.string.isRequired,
+};
+export default RocketItem;
