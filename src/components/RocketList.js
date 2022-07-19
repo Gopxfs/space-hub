5128030dd0567775e504d639e1d001c91bbeb41b
worashf <uppert83@gmail.com>
2022-07-20 00:19:40 +0300

    render rockets

--- src/components/RocketList.js
+++ src/components/RocketList.js
@@ -1,36 +1,31 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { Container } from 'react-bootstrap';
-import axios from 'axios';
+
 import Rocket from './RocketItem';
+import { getRockets } from '../redux/rockets/rockets';
 
 const RocketList = () => {
-  const [rockets, setRockets] = useState([]);
-  const getData = async () => {
-    const response = await axios.get('https://api.spacexdata.com/v3/rockets');
-    const mappedRockets = response.data.map((rocket) => ({
-      id: rocket.rocket_id,
-      name: rocket.rocket_name,
-      type: rocket.rocket_type,
-      images: rocket.flickr_images[0],
-      description: rocket.description,
-    }));
-    console.log(mappedRockets);
-    setRockets(mappedRockets);
-  };
+  const { rockets } = useSelector((state) => state.rocket);
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    getData();
+    dispatch(getRockets());
   }, []);
   return (
     <Container fluid>
-      {rockets.map((roc) => (
-        <Rocket
-          key={roc.id}
-          name={roc.name}
-          thumb={roc.images}
-          description={roc.description}
-        />
-      ))}
+      {rockets.length ? (
+        rockets.map((roc) => (
+          <Rocket
+            key={roc.id}
+            name={roc.name}
+            thumb={roc.images}
+            description={roc.description}
+          />
+        ))
+      ) : (
+        <div className="">nodata</div>
+      )}
     </Container>
   );
 };
