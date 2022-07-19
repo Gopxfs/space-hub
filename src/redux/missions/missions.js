925eff9c470af3efe8335a2224d3a028d01d92e2
Gabriel <gopxfs@hotmail.com>
2022-07-19 01:00:28 -0300

    Add missionSlice structure and getData logic
    Add name, initialState containing arrays of: mission_id, mission_name and description, and getData reducer, that gets the data fetched form the api and update the 3 arrays

--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -1,5 +1,31 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const missionSlice = createSlice({});
+const missionSlice = createSlice({
+  name: 'missions',
+  initialState: {
+    mission_id: [],
+    mission_name: [],
+    description: [],
+  },
+  reducers: {
+    getData(state, { payload }) {
+      const ids = [];
+      const names = [];
+      const descriptions = [];
+      payload.forEach((mission) => {
+        ids.push(mission.mission_id);
+        names.push(mission.mission_name);
+        descriptions.push(mission.description);
+      });
+      return {
+        mission_id: ids,
+        mission_name: names,
+        description: descriptions,
+      };
+    },
+  },
+});
+
+export const { getData } = missionSlice.actions;
 
 export default missionSlice.reducer;
