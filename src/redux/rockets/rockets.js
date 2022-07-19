962891e345cfef1df49c55478ea9525728f8cfed
worashf <uppert83@gmail.com>
2022-07-20 00:21:03 +0300

    add fetch data and reducer

--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -1,5 +1,40 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+/* eslint-disable */
+export const getRockets = createAsyncThunk('rockets/getRockets', async () => {
+  try {
+    const res = await axios.get('https://api.spacexdata.com/v3/rockets');
 
-const rockectSlice = createSlice({});
+    const mappedRockets = res.data.map((rocket) => ({
+      id: rocket.id,
+      name: rocket.rocket_name,
+      images: rocket.flickr_images[0],
+      description: rocket.description,
+    }));
+    return mappedRockets;
+  } catch (err) {
+    throw new Error(err);
+  }
+});
+
+const rockectSlice = createSlice({
+  name: 'rockets',
+  initialState: {
+    rockets: [],
+    loading: 'idle',
+  },
+  extraReducers: {
+    [getRockets.pending]: (state) => {
+      state.loading = 'loading';
+    },
+    [getRockets.fulfilled]: (state, { payload }) => {
+      state.loading = 'success';
+      state.rockets = payload;
+    },
+    [getRockets.rejected]: (state) => {
+      state.loading = 'failed';
+    },
+  },
+});
 
 export default rockectSlice.reducer;
