64841a1ab1a5982b0c10cb6ee1d83c1a69c1f236
Gabriel <gopxfs@hotmail.com>
2022-07-19 18:29:40 -0300

    Remove console.log

--- src/components/RocketList.js
+++ src/components/RocketList.js
@@ -14,7 +14,6 @@ const RocketList = () => {
       images: rocket.flickr_images[0],
       description: rocket.description,
     }));
-    console.log(mappedRockets);
     setRockets(mappedRockets);
   };
 
