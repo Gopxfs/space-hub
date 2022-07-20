988c2dfc702ba00aaf4f6277696fd83cc76ef3c9
worashf <uppert83@gmail.com>
2022-07-20 19:36:19 +0300

    add  id props and disptch

--- src/components/RocketList.js
+++ src/components/RocketList.js
@@ -6,28 +6,26 @@ import Rocket from './RocketItem';
 import { getRockets } from '../redux/rockets/rockets';
 
 const RocketList = () => {
-  const { rockets } = useSelector((state) => state.rocket);
+  const rockets = useSelector((state) => state.rocket);
   const dispatch = useDispatch();
 
   useEffect(() => {
     if (!rockets.length) {
       dispatch(getRockets());
     }
-  }, []);
+  }, [dispatch]);
   return (
     <Container fluid>
-      {rockets.length ? (
-        rockets.map((roc) => (
-          <Rocket
-            key={roc.id}
-            name={roc.name}
-            thumb={roc.images}
-            description={roc.description}
-          />
-        ))
-      ) : (
-        <div className="">nodata</div>
-      )}
+      {rockets.map((roc) => (
+        <Rocket
+          key={roc.id}
+          id={roc.id}
+          name={roc.name}
+          thumb={roc.images}
+          description={roc.description}
+          reserved={roc.reserved}
+        />
+      ))}
     </Container>
   );
 };
